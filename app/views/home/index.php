7e2f95de5f1d1a0a135fbd4aa486424ae6164ffe
Harmanjot14 <jotkharman@gmail.com>
2025-07-15 04:00:48 +0000

    Added class page-container to fix the space in home page

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,5 +1,6 @@
 <?php require_once 'app/views/templates/header.php' ?>
 <div class="home">
+    <div class="page-container">
     <div class="page-header" id="banner">
         <div class="row">
             <div>
@@ -14,7 +15,7 @@
             <p> <a href="/logout">Click here to logout</a></p>
         </div>
     </div>
-
+</div>
 </div>
 
 
