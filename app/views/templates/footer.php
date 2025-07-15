fac7a5b12445bffa45a6904c9288d37822e3bc77
Harmanjot14 <jotkharman@gmail.com>
2025-07-15 03:36:40 +0000

    Added event listener for contact info and about

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -2,18 +2,29 @@
 <footer class="footer">    
     <div class="footer-content">
         <ul class="footer-links">
-            <li><a href="#" id="contact-toggle" class="mx-2">Contact</a></li>
+            <li><a href="#" id="contact-toggle" class="mx-2">Contact Info</a></li>
+            <div id="contact-info" style="display: none;" class="mt-2">
+                <p>Email: harmanjotkaur005@algomau.ca</p>
+            </div>
+            <li><a href = "#" id = "about-toggle" class="mx-2">About</a></li>
+            <div id="about-info" style="display: none;" class="mt-2">
+                <p>A simple reminder app built using PHP and MySQL during the course Web Data Management</p>
+            </div>
             <li><a href="https://www.linkedin.com/in/harmanjot-kaur-41b6b7180/">LinkedIn</a></li>
             <li><a href="https://github.com/Harmanjot14">GitHub</a></li>
-            <li><a href = "#" id = "about-toggle" class="mx-2">About</a></li>
         </ul>
         <p>Harmanjot Kaur &copy; <?php echo date('Y'); ?> </p>
-        <div id="contact-info" style="display: none;" class="mt-2">
-            <p>Email: harmanjotkaur005@algomau.ca</p>
-        </div>
-        <div id="about-info" style="display: none;" class="mt-2">
-            <p>This is a simple reminder app built using PHP and MySQL</p>
-        </div>
+       
+        <script>
+            document.getElementById('contact-toggle').addEventListener('click', function(){
+                var contactInfo = document.getElementById('contact-info');
+                contactInfo.style.display = (contactInfo.style.display === 'none') ? 'block' : 'none';
+            });
+            document.getElementById('about-toggle').addEventListener('click', function(){
+                var aboutInfo = document.getElementById('about-info');
+                aboutInfo.style.display = (aboutInfo.style.display === 'none') ? 'block' : 'none';
+            });
+        </script>                                                              
     </div>
 </footer>
 
