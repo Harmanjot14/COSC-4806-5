5016fe75137d9cded87bbe2721a7471b874d9fa2
Harmanjot14 <jotkharman@gmail.com>
2025-07-15 03:20:04 +0000

    Updated footer
    Added contact, linkdin, github, my name, about

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,7 +1,19 @@
 
 <footer class="footer">    
-    <div class="row">
-            <p>Copyright &copy; <?php echo date('Y'); ?> </p>
+    <div class="footer-content">
+        <ul class="footer-links">
+            <li><a href="#" id="contact-toggle" class="mx-2">Contact</a></li>
+            <li><a href="https://www.linkedin.com/in/harmanjot-kaur-41b6b7180/">LinkedIn</a></li>
+            <li><a href="https://github.com/Harmanjot14">GitHub</a></li>
+            <li><a href = "#" id = "about-toggle" class="mx-2">About</a></li>
+        </ul>
+        <p>Harmanjot Kaur &copy; <?php echo date('Y'); ?> </p>
+        <div id="contact-info" style="display: none;" class="mt-2">
+            <p>Email: harmanjotkaur005@algomau.ca</p>
+        </div>
+        <div id="about-info" style="display: none;" class="mt-2">
+            <p>This is a simple reminder app built using PHP and MySQL</p>
+        </div>
     </div>
 </footer>
 
