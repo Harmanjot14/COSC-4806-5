0c0474487e3b71f6704c26558e584d9e3a39c595
Harmanjot14 <jotkharman@gmail.com>
2025-07-16 00:40:28 +0000

    Added  view/homePublic/index.php for non logged in users

---
+++ app/views/homePublic/index.php
@@ -0,0 +1,17 @@
+<?php require_once 'app/views/templates/headerPublic.php' ?>
+
+<div class="home">
+    <div class="page-container">
+        <div class="page-header" id="banner">
+            <div class="row">
+                <div>
+                    <h1>Welcome to our reminders app!</h1>
+                    <p class="lead"> <?= date("F jS, Y"); ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<?php require_once 'app/views/templates/footer.php' ?>
