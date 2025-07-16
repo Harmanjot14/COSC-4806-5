4254027c51e605739a7fdb9a3607094dfefd0d90
Harmanjot14 <jotkharman@gmail.com>
2025-07-16 01:04:18 +0000

    Added little Home Public page information about the app

--- app/views/homePublic/index.php
+++ app/views/homePublic/index.php
@@ -5,8 +5,25 @@
         <div class="page-header" id="banner">
             <div class="row">
                 <div>
-                    <h1>Welcome to our reminders app!</h1>
-                    <p class="lead"> <?= date("F jS, Y"); ?></p>
+                    <h1>Welcome!</h1>
+                    <ul class="public-list">
+                        <li>Plan your daily tasks with the help of our reminder app</li>
+                        <br>
+                        <li>To track your task progress you can use buttons: 
+                            <ul class="sub-list">
+                                <li><strong>Add button:</strong> to add a new task</li>
+                                <li><strong>Edit button:</strong> to edit an existing task</li>
+                                <li><strong>Delete button:</strong> to delete an existing task</li>
+                                <li><strong>Complete button:</strong> to mark a task as completed</li>
+                            </ul>
+                        </li>
+                        <br>
+                        <li>If you are already a user, Please login to your account to start using our app</li><br>
+                        <li>If you are a new user, Please sign up to create an account to start using our app</li>
+                    </ul>
+                    <br><br><br>
+                    
+                    <p class="lead"><strong><?= date("F jS, Y"); ?></strong> </p>
                 </div>
             </div>
         </div>
