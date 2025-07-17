a5736a7e8007cc441c85e6b6e6832a85087b1da7
Harmanjot14 <jotkharman@gmail.com>
2025-07-17 23:24:04 +0000

    In front of class name added "css-"

--- app/views/templates/headerPublic.php
+++ app/views/templates/headerPublic.php
@@ -17,13 +17,13 @@ if (isset($_SESSION['auth']) == 1) {
     <link rel="stylesheet" href="/style.css">
 </head>
 <body>
-    <nav class="navbar">
-        <div class="container">
-          <a class="navbar-brand" href="#">COSC 4806</a>
+    <nav class="css-navbar">
+        <div class="css-container">
+          <a class="css-navbar-brand" href="#">COSC 4806</a>
           <ul>
-            <li class="nav-item"><a href="/homePublic">Home</a></li>
-            <li class="nav-item"><a href="/login">Login</a></li>
-            <li class="nav-item"><a href="/create">SignUp</a></li>
+            <li class="css-nav-item"><a href="/homePublic">Home</a></li>
+            <li class="css-nav-item"><a href="/login">Login</a></li>
+            <li class="css-nav-item"><a href="/create">SignUp</a></li>
           </ul>
         </div>
     </nav>
