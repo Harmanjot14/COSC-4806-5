7bbf9b8348750d148a250b23ca99324d617d8f59
Harmanjot14 <jotkharman@gmail.com>
2025-07-17 01:34:22 +0000

    Added breadcrumb Bootstrap component

--- app/views/reports/index.php
+++ app/views/reports/index.php
@@ -1,6 +1,14 @@
 <?php require_once 'app/views/templates/header.php' ?>
 
 <div class="reports">
+    <!-- breadcrumb -->  
+    <nav aria-label="breadcrumb">
+      <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="/">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Reports</li>
+      </ol>
+    </nav>
+ 
     <div class="page-header" id="banner">
       <h1>Reports</h1>
       <div class="row">
