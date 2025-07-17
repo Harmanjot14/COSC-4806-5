67271d595e0a5dae8518317147d7d0b19a3cfa25
Harmanjot14 <jotkharman@gmail.com>
2025-07-17 01:10:10 +0000

    Using now only Report model and Changed function from get_all_reminders to get_all_reminders_with_username

--- app/controllers/reports.php
+++ app/controllers/reports.php
@@ -5,15 +5,16 @@
     public function index() {		
       session_start();
       
-      $reminder = $this->model('Reminder');
-      $reminders_list = $reminder->get_all_reminders();
-
       $report = $this->model('Report');
+      $reminders_list = $report->get_all_reminders_with_username();
+
       $most_reminders = $report->get_user_with_most_reminders();
+
+      $username = $_SESSION['username'] ?? null;
+      $total_logins = $username ? $report->get_total_logins_by_username($username):0;
       
-      $this->view('reports/index', ['reminders' => $reminders_list, 'mostReminders' => $most_reminders]);
+      $this->view('reports/index', ['reminders' => $reminders_list, 'mostReminders' => $most_reminders, 'totalLogins' => $total_logins]);
       
-    
     }
   }
 
