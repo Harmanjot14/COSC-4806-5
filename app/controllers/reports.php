1cd04f81fdbc6017db300ef5b93820075ece7ba0
Harmanjot14 <jotkharman@gmail.com>
2025-07-17 03:44:56 +0000

    Fixed total login attempt

--- app/controllers/reports.php
+++ app/controllers/reports.php
@@ -9,9 +9,7 @@
       $reminders_list = $report->get_all_reminders_with_username();
 
       $most_reminders = $report->get_user_with_most_reminders();
-
-      $username = $_SESSION['username'] ?? null;
-      $total_logins = $username ? $report->get_total_logins_by_username($username):0;
+      $total_logins = $report->get_total_logins_by_username();
       
       $this->view('reports/index', ['reminders' => $reminders_list, 'mostReminders' => $most_reminders, 'totalLogins' => $total_logins]);
       
