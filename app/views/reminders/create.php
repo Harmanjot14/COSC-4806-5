1e5372200d1c1ecfa6e3c17e462be0e62b0b5ce9
Harmanjot14 <jotkharman@gmail.com>
2025-07-17 01:40:26 +0000

    Added breadcrumb for create reminder page

--- app/views/reminders/create.php
+++ app/views/reminders/create.php
@@ -1,21 +1,33 @@
 <?php require_once 'app/views/templates/header.php' ?>
 
 <div class="create_reminder">
-  <div class="page-header" id="banner">
-      <div class="row">
-        <div>
-           <h1>Create Reminder</h1>
-           <form action="/reminders/create" method="post">
-              <label for="subject">Subject:</label>
-              <input type="text" id="subject" name="subject">
-              <br><br>
-              <input type="submit" value="Submit">
-           </form>
-           <br><br>
-           <a href = "/reminders"><button>Go Back</button></a>
+  <!-- breadcrumb -->  
+  <nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+      <li class="breadcrumb-item"><a href="/">Home</a></li>
+      <li class="breadcrumb-item"><a href="/reminders">Reminders</a></li>
+      <li class="breadcrumb-item active" aria-current="page">Create</li>
+    </ol>
+  </nav>
+  
+  <div class="page-container">
+    <div class="page-header" id="banner">
+        <div class="row">
+          <div>
+             <h1>Create Reminder</h1>
+             <form action="/reminders/create" method="post">
+                <label for="subject">Subject:</label>
+                <input type="text" id="subject" name="subject">
+                <br><br>
+                <input type="submit" value="Submit">
+             </form>
+             <br><br>
+             <a href = "/reminders"><button>Go Back</button></a>
+          </div>
         </div>
-      </div>
-    </div>    
+      </div> 
+    </div>
+  </div>
     
   
-    <?php require_once 'app/views/templates/footer.php' ?>
+  <?php require_once 'app/views/templates/footer.php' ?>
