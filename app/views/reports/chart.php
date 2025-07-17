209e2e4eafce66768d222ae5d15afbebbabc7129
Harmanjot14 <jotkharman@gmail.com>
2025-07-17 03:29:32 +0000

    Added Second chart

--- app/views/reports/chart.php
+++ app/views/reports/chart.php
@@ -17,6 +17,8 @@
       <div class="page-header" id="banner">
         <h1>Charts</h1>
         <br>
+        
+        <!--First Chart-->
         <div class="row">
           <div class="col-md-6">
             <div class="card mb-4">
@@ -26,7 +28,6 @@
                 <!-- Chart.js bar chart -->
                 <canvas id="myChart"></canvas>
 
-                
                 <!-- Chart.js script -->
                 <script>
                   const ctx = document.getElementById('myChart');
@@ -71,10 +72,72 @@
             </div>
           </div>
         </div>      
-      </div>  
-    </div>
+      <br><br><br>
 
-  
+      <!--Second Chart-->
+      <div class="row">
+        <div class="col-md-6">
+          <div class="card mb-4">
+            <div class="card-header">Login Attempts per User</div>
+            <div class="card-body">
+              <div class="chart-container">
+                <!-- Chart.js bar chart -->
+                <canvas id="myChart2"></canvas>
+                <!-- Chart.js script -->
+                <script>
+                  const ctx2 = document.getElementById('myChart2');
+                  const myChart2 = new Chart(ctx2, {
+                      // Set the chart type to bar
+                      type: 'bar',
+                    
+                      // Set the data for the chart
+                      data: {
+                          labels: [<?php foreach ($data['login_attempts'] as $login_attempt) { echo "'" . $login_attempt['username'] . "',"; } ?>],
+                        
+                          datasets: [{
+                              label: 'Good Attempts',
+                              data: [<?php foreach ($data['login_attempts'] as $login_attempt) { echo $login_attempt['good_attempts'] . ","; } ?>],
+                              backgroundColor: 'rgba(0, 128, 0, 0.4)',
+                              borderColor: 'rgba(0, 128, 0, 1)',
+                              borderWidth: 1
+                          },
+                          {
+                              label: 'Bad Attempts',
+                              data: [<?php foreach ($data['login_attempts'] as $login_attempt) { echo $login_attempt['bad_attempts'] . ","; }?>],    
+                              backgroundColor: 'rgba(255, 0, 0, 0.4)',
+                              borderColor: 'rgba(255, 0, 0, 1)',
+                              borderWidth: 1
+                          }]
+                      },
+
+                     // Set the options for the chart
+                      options: {
+                          scales: {
+                              y: {
+                                  beginAtZero: true
+                              }
+                          },
+                          plugins: {
+                              legend:{
+                                  display: true
+                              },
+                              title: {
+                                  display: true,
+                                  text: 'Login Attempts per User'  
+                              }
+                          }
+                      }
+                  });
+                  </script>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+                    
+                    
+      </div>            
+    </div>  
 </div>
 
 
